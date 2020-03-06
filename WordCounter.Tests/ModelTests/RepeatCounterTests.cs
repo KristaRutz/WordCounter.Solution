5e1caa25a62c3c2afbc93eb4ccf64b9c97b11eaa
Krista Rutz <rutzkri000@gmail.com>
2020-03-06 10:39:15 -0800

    add three tests to account for the Validate() feature that was added when the Matches() method got refactored

--- WordCounter.Tests/ModelTests/RepeatCounterTests.cs
+++ WordCounter.Tests/ModelTests/RepeatCounterTests.cs
@@ -14,21 +14,42 @@ namespace WordCounter.Tests
     }
 
     [TestMethod]
-    public void RepeatCounter_IdentifiesEmptyWordInput_Zero()
+    public void Matches_IdentifiesEmptyWordInput_Zero()
     {
       RepeatCounter example = new RepeatCounter("", "test");
       Assert.AreEqual(0, example.Matches());
     }
 
     [TestMethod]
-    public void RepeatCounter_IdentifiesEmptyStringInput_Zero()
+    public void Matches_IdentifiesEmptyStringInput_Zero()
     {
       RepeatCounter example = new RepeatCounter("test", "");
       Assert.AreEqual(0, example.Matches());
     }
 
     [TestMethod]
-    public void RepeatCounter_SingleWordSingleStringInput_One()
+    public void Validate_IdentifiesEmptyWordInputs_False()
+    {
+      RepeatCounter emptyWord = new RepeatCounter("", "test");
+      Assert.AreEqual(false, emptyWord.Validate());
+    }
+
+    [TestMethod]
+    public void Validate_IdentifiesEmptyStringInputs_False()
+    {
+      RepeatCounter emptySearchStr = new RepeatCounter("test", "");
+      Assert.AreEqual(false, emptySearchStr.Validate());
+    }
+
+    [TestMethod]
+    public void Validate_IdentifiesNonEmptyInputs_True()
+    {
+      RepeatCounter example = new RepeatCounter("test", "test");
+      Assert.AreEqual(true, example.Validate());
+    }
+
+    [TestMethod]
+    public void Matches_SingleWordIdenticalStringInput_One()
     {
       RepeatCounter example = new RepeatCounter("test", "test");
       RepeatCounter example2 = new RepeatCounter("Hello", "Hello");
@@ -37,21 +58,21 @@ namespace WordCounter.Tests
     }
 
     [TestMethod]
-    public void RepeatCounter_SingleWordMultiSameStringInput_Int()
+    public void Matches_StringMadeOfOnlyWordInput_Int()
     {
       RepeatCounter example = new RepeatCounter("test", "test test test");
       Assert.AreEqual(3, example.Matches());
     }
 
     [TestMethod]
-    public void RepeatCounter_CheckFrequencyOfInput_Int()
+    public void Matches_CheckFrequencyOfInput_Int()
     {
       RepeatCounter example = new RepeatCounter("test", "this test is a test");
       Assert.AreEqual(2, example.Matches());
     }
 
     [TestMethod]
-    public void RepeatCounter_CheckFrequencyOfPerfectMatch_Int()
+    public void Matches_CheckFrequencyOfPerfectMatch_Int()
     {
       RepeatCounter example = new RepeatCounter("test", "this test is still testing");
       Assert.AreEqual(1, example.Matches());
